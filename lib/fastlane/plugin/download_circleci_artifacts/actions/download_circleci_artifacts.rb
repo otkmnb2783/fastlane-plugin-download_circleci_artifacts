2eb0c4689fc9feae43465f413eb85e05024a6266
Manabu OHTAKE <manabu2783@hotmail.com>
2017-02-19 14:59:08 +0900

    improved: download action * remove other plugin dependencies * show download progress

--- lib/fastlane/plugin/download_circleci_artifacts/actions/download_circleci_artifacts.rb
+++ lib/fastlane/plugin/download_circleci_artifacts/actions/download_circleci_artifacts.rb
@@ -1,12 +1,18 @@
-require 'circleci'
-
 module Fastlane
   module Actions
     class DownloadCircleciArtifactsAction < Action
       def self.run(params)
+        require 'circleci'
+        require 'open-uri'
+        require 'fileutils'
         Actions.verify_gem!('circleci')
         configure(params)
-        get(params)
+        artifacts = get(params)
+        artifacts.each do |artifact|
+          destination_path = "#{@dist}/#{artifact[:file]}"
+          File.delete(destination_path) if File.exist?(destination_path)
+          download_artifact(artifact[:url].to_s, destination_path)
+        end
       end
 
       def self.configure(params)
@@ -33,15 +39,57 @@ module Fastlane
         unless artifacts and !artifacts.empty?
           UI.user_error! "Not Found Artifact download url! 🚀"
         end
-        artifacts.each do |artifact|
-          download_url = "#{artifact[:url]}?circle-token=#{@token}"
-          destination_path = "#{@dist}/#{artifact[:file]}"
-          UI.message download_url
-          File.delete(destination_path) if File.exist?(destination_path)
-          Actions::DownloadFileAction.run(
-            url: download_url,
-            destination_path: destination_path
-          )
+        artifacts
+      end
+
+      def self.download_artifact(artifact_url, destination_path)
+        UI.important "Download artifact url #{artifact_url}"
+        request_url = "#{artifact_url}?circle-token=#{@token}"
+        send_download_request(request_url, "fastlane-plugin_download_circleci_artifacts", "application/octet-stream", destination_path)
+        puts "finish"
+        compressed_file_size = File.size(destination_path).to_f / 2**20
+        formatted_file_size = format('%.2f', compressed_file_size)
+        UI.success("Download finished, total size: #{formatted_file_size} MB ✅")
+      rescue => ex
+        UI.user_error!("Error fetching release's artifact: #{ex}")
+      end
+
+      def self.send_download_request(request_url, user_agent, accept, destination_path)
+        step = 0
+        partial = 0
+        progress = 0
+        File.open(destination_path, "wb") do |saved_file|
+          # the following "open" is provided by open-uri
+          open(request_url, "User-Agent" => user_agent, "Accept" => accept, :content_length_proc => lambda do |t|
+            if t && 0 < t
+              step = t / 10
+              partial = step
+              formatted_file_size = format('%.2f', t.to_f / 2**20)
+              UI.important("Total size: #{formatted_file_size} MB")
+            else
+              partial = 5 * 1024 * 1024
+            end
+          end,
+          :progress_proc => lambda do |s|
+            if s > partial
+              if step.zero?
+                puts '.'
+                partial = partial + (5 * 1024 * 1024)
+                formatted_file_size = format('%.1f', s.to_f / 2**20)
+                UI.message("download size: #{formatted_file_size} MB")
+              else
+                partial += step
+                UI.message "#{progress}%"
+                progress = (partial / step) * 10
+              end
+            else
+              if step.zero?
+                print '.'
+              end
+            end
+          end) do |read_file|
+            saved_file.write(read_file.read)
+          end
         end
       end
 
